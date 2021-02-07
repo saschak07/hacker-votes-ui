e7a1a90eef4e87e8973fead5c4af994851228bb6
saschak07 <saschak07@gmail.com>
2021-02-07 13:42:38 +0530

    removing unused dependencies

--- src/components/dashBoard/dashBoard.js
+++ src/components/dashBoard/dashBoard.js
@@ -1,4 +1,4 @@
-import React,{useState,useEffect,useContext} from 'react'
+import React,{useState,useEffect} from 'react'
 import Navbar from '../navbar/navbar'
 import HackerDetails from '../hackerDetails/hackerDetails';
 import HackersData from '../hackersData/hackersData'
