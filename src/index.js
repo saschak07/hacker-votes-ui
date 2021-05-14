62533fa337668c429f24a304bb48dc20823d3f2a
saschak07 <saschak07@gmail.com>
2021-05-14 18:00:30 +0530

    added persistant redux for solving page refresh

--- src/index.js
+++ src/index.js
@@ -4,13 +4,27 @@ import './index.css';
 import { Provider } from "react-redux";
 import { createStore } from "redux";
 import reducer from './components/store/reducer';
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-const store = createStore(reducer)
+import { PersistGate } from 'redux-persist/integration/react'
+import Loader from './components/loader/loader'
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, reducer)
+const store = createStore(persistedReducer)
+const persistor = persistStore(store)
 ReactDOM.render(
   <React.StrictMode>
     <Provider store = {store}>
-    <App />
+      <PersistGate loading={<Loader/>} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
